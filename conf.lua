abb987678adff0ea71571f78704665030916e51f
li-mz26 <li-mz26@users.noreply.github.com>
2026-02-20 00:21:23 +0800

    Add LÖVE config file

---
+++ conf.lua
@@ -0,0 +1,8 @@
+-- LÖVE Configuration File
+function love.conf(t)
+    t.window.title = "Lua Roguelike RPG"
+    t.window.width = 800
+    t.window.height = 600
+    t.window.resizable = false
+    t.console = true -- Enable console for debugging (Windows only)
+end
